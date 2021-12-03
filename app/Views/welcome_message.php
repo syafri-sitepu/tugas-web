5d0fa3a1a7d59c3d20a2ecf0f12752a833d6b736
syafri-sitepu <syapri.sitepu@gmail.com>
2021-12-03 12:27:59 +0700

    manggil css

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -14,7 +14,7 @@
     <title>Universitas Pancabudi</title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
     <a class="navbar-brand" href="#">UNPAB</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
