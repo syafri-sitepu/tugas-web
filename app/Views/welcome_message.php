52d64776b80ca36c5539a367061247618b92502e
syafri-sitepu <syapri.sitepu@gmail.com>
2021-12-03 12:37:13 +0700

    manggil css

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -8,13 +8,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-	<!-- my css -->
-	<link rel="stylesheet" href="style.css">
+	
 
     <title>Universitas Pancabudi</title>
   </head>
   <body>
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
     <a class="navbar-brand" href="#">UNPAB</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
